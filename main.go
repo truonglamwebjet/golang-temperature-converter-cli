077ec61022e693314b4e3475d495b96f8210eab9
truong-lam-webjet <truong.lam@webjet.com.au>
2022-03-01 09:49:48 +1100

    m1

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -27,6 +28,12 @@ func main() {
 			fmt.Println("Good bye!")
 			break
 		}
+
+		if len(os.Args) != 2 {
+			printError(errReadingInput)
+		}
+
+		originUnit = strings.ToUpper(os.Args[1])
 	}
 }
 
